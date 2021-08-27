fca218b296cf230d0714490d8ea7cc6908e87e58
SaraPasinato <pasinatosara.sp@gmail.com>
2021-08-27 13:13:59 +0200

    add v-model input text message

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // !Milestone 2
 //* Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
-// TODO:Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// TODO:Click sul contatto mostra la conversazione del contatto cliccato
+//* Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+//* Click sul contatto mostra la conversazione del contatto cliccato
 //! EXTRA:
-// Per chi avesse tempo e voglia di portarsi avanti, questa è la prossima milestone:
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// !Milestone 3:
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra,
+//   come messaggio verde
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo
 console.log("HERE",Vue);
 
@@ -17,7 +18,10 @@ var app = new Vue({
     el: '#app',
     data: {
       data,
+      //tiene traccia della posizione corrente in array contacts
       currentContact:0,
+      currentText:"",      //tiene traccia del testo in input
+
     },
     methods:{
       //metodo per settare poi il contatto al click sul profilo (user-contact)
